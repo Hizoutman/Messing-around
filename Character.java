c5c44521075ab0be1873cf1d7a04a28c997e5834
smantri7 <smantri7@gatech.edu>
2015-07-28 14:44:07 -0400

    Create Character.java
    Same as enemy. Check to make sure all elements are there.

---
+++ Character.java
@@ -0,0 +1,36 @@
+public class Character {
+
+    private String name; //Add input scanner later//
+    private String type;
+    private int health;
+    private int attack;
+    private int defense;
+
+    public Character(String name,String type,int attack,int defense) {
+        this.name = name; //This will need an input scanner later//
+        this.type = type;
+        this.attack = attack;
+        this.defense = defense;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public int getAttack() {
+        return this.attack;
+    }
+
+    public int getDefense() {
+        return this.defense;
+    }
+
+    public int attack(Enemy e) {
+        int damage = this.getAttack() - e.getDefense();
+        return damage;
+    }
+}
