ce81eb26ade2f113ff47b14f546f930f905a6979
smantri7 <smantri7@gatech.edu>
2015-07-28 14:42:57 -0400

    Created Enemy.java
    Please check for errors. No compiling errors. If I missed something that an enemy needs, add it.

---
+++ Enemy.java
@@ -0,0 +1,35 @@
+public class Enemy {
+
+    private int health;
+    private String name;
+    private int attack;
+    private int defense;
+
+    public Enemy(String name,int health,int attack,int defense) {
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+        this.defense = defense;
+    }
+
+    public int getHealth() {
+        return this.health;
+    }
+
+    public void setHealth(int h) {
+        this.health = h;
+    }
+
+    public int getAttack() {
+        return this.attack;
+    }
+
+    public int getDefense() {
+        return this.defense;
+    }
+
+    public int attack(Character c) {
+        int damage = getAttack() - c.getDefense();
+        return damage;
+    }
+}
