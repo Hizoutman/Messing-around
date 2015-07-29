b8fb3028eccf8cc6853b132158947878a72054b2
rzd83091 <rzd83091@uga.edu>
2015-07-28 21:34:53 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -9,10 +9,10 @@ public class Driver{
   Scanner input = new Scanner(System.in);
   System.out.println("Welcome, please enter in a name");
     name = input.next();
-  System.out.println("Select a character class from the list:\n1)Warrior Str:9 Def:1 " +
-  "\n2)Mage Str:4 Def:6 "+
-  "\n3)Thief Str:7 Def:3 "+
-  "\n4)Priest" Str:2 Def:8);
+  System.out.println("Select a character class from the list:\n1)Warrior HP:100 Str:9 Def:1 " +
+  "\n2)Mage HP:80 Str:4 Def:6 "+
+  "\n3)Thief HP:76 Str:7 Def:3 "+
+  "\n4)Priest" HP:94 Str:2 Def:8);
     while(true){
       choice = input.nextInt();
       if(choice<1 || choice>4){
@@ -21,10 +21,10 @@ public class Driver{
       else break;
     }
   Character player = new Character();
-  if(choice==1)player.Character(name,"Warrior",9,1);
-  if(choice==2)player.Character(name,"Mage",4,6);;
-  if(choice==3)player.Character(name,"Thief",7,3);;
-  if(choice==4)player.Character(name,"Priest",2,8);;
+  if(choice==1)player.Character(name,"Warrior",100,9,1,0);
+  if(choice==2)player.Character(name,"Mage",80,4,6,0);;
+  if(choice==3)player.Character(name,"Thief",76,7,3,0);;
+  if(choice==4)player.Character(name,"Priest",94,2,8,0);;
   input.close()
   }
 
