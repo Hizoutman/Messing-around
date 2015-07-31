9c4cfe433a69391741c29550acb8da0bd6af8c4b
rzd83091 <rzd83091@uga.edu>
2015-07-31 19:22:09 -0400

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,19 +1,16 @@
 public class Character {
 
     private String name; //Add input scanner later//
-    private String type;
     private int health;
     private int attack;
     private int defense;
-    private int pos;
 
-    public Character(String name,String type, int health,int attack,int defense,int pos) {
+    public Character(String name, int health,int attack,int defense) {
         this.name = name; //This will need an input scanner later//
         this.type = type;
         this.health = health;
         this.attack = attack;
         this.defense = defense;
-        this.pos = pos;
     }
 
     public String getName() {
@@ -43,11 +40,7 @@ public class Character {
     public void setDefense(int d) {
         this.defense = d;
     }
-
-    public void setPos(int p) {
-        this.pos = p;
-    }
-
+/*
     public int attack(Enemy e) {
         int damage = this.getAttack() - e.getDefense();
         if(damage <= 0) {
@@ -55,4 +48,5 @@ public class Character {
         }
         return damage;
     }
+*/
 }
