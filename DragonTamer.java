a2919b3858816346a84a141a981a0a5b794b0a7d
smantri7 <smantri7@gatech.edu>
2015-07-31 20:36:13 -0400

    Create DragonTamer.java

---
+++ DragonTamer.java
@@ -0,0 +1,19 @@
+public class DragonTamer extends Character {
+    private String special;
+
+    public DragonTamer(String name,int health, int attack,int defense) {
+        super(name,health,attack,defense);
+        this.special = "Dragon Solar Strike";
+    }
+
+    public String getSpecial() {
+        return this.special;
+    }
+
+    public void special(Character enemy) {
+        System.out.println("Dragon Tamer used special move!");
+        //"Critical Strike ratio at 100%"//
+        //"Enemy's defense temporarily set to 0"//
+        this.attack(enemy);
+    }
+}
