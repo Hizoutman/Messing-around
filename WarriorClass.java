4b61e28fe09c81a2bde106f332d2c3e9bb6bc06f
rzd83091 <rzd83091@uga.edu>
2015-07-31 20:06:54 -0400

    Update WarriorClass.java

--- WarriorClass.java
+++ WarriorClass.java
@@ -2,11 +2,14 @@ public class WarriorClass extends Character{
   private String special;
   
     public WarriorClass(String name, int health,int attack,int defense){
-    super();
+    super(name,health,attack,defense);
     this.special = "Bash";
-    //crit attack/low hit rate
     }
     public String getSpecial(){
       return this.special;
+    }
+    public void Special(Character a){
+      //crit attack/low hit rate
+      attack(a);
     }
 }
