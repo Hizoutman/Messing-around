06c91531d1ead4a66371425fc78cc626cfb29ff6
smantri7 <smantri7@gatech.edu>
2015-08-02 13:03:39 -0400

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,15 @@
+public class Item {
+    private String type; //heal,hurt,atk,def
+    private int amt;
+    private String name;
+
+    public Item(String name, String type,int amt) {
+        this.name = name;
+        this.type = type;
+        this.amt = amt;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+}
