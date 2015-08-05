105fac21559da1718bd28441e4b39985da68f9e3
smantri7 <smantri7@gatech.edu>
2015-08-05 18:44:47 -0400

    Create Animate.java

---
+++ GUIs/Animate.java
@@ -0,0 +1,14 @@
+//IMPORTANT IMPORT STATEMENTS//
+import javafx.animation.Interpolator;
+import javafx.animation.Transition;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+public class Animate {
+    public Animate() {}
+    public void right() {}
+    public void left() {}
+    public void up() {}
+    public void down() {}
+ }
